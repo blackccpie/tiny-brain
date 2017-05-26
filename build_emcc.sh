63b7517608c0b1307014c72f736dd3eecccaa343
blackccpie <blackccpie@free.fr>
2017-05-26 17:06:36 +0200

    - emcc improvements.

--- build_emcc.sh
+++ build_emcc.sh
@@ -3,5 +3,5 @@
 mkdir -p build_emcc
 
 cd build_emcc
-cmake -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake -DUSE_AVX2=ON -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake -DUSE_EMSCRIPTEN=ON -DCMAKE_BUILD_TYPE=Release ..
 make -j3
