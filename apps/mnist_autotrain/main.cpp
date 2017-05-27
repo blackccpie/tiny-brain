8f7502f0a6b02aa04b017d4633ef2420f6e0f00f
blackccpie <blackccpie@free.fr>
2017-05-27 16:13:42 +0200

    - added configurable border size. - fixed seed, reaches 98.4 score after 5 epochs. - misc improvements.

--- apps/mnist_autotrain/main.cpp
+++ apps/mnist_autotrain/main.cpp
@@ -25,6 +25,7 @@ THE SOFTWARE.
 #include "tiny_dnn/tiny_dnn.h"
 
 #include "adamax.h"
+//#include "mnist_csv_parser.h"
 
 #include <iostream>
 
@@ -32,19 +33,18 @@ using namespace tiny_dnn;
 using namespace tiny_dnn::layers;
 using namespace tiny_dnn::activation;
 
+static const int border_px = 2;
+
 static void construct_net(  network<sequential> &nn, core::backend_t backend_type )
 {
     // construct nets
     //
-    // C : convolution
-    // S : sub-sampling
-    // F : fully connected
     // clang-format off
-    nn  << conv( 32, 32, 5, 1, 12 ) << relu() // C1, 1@32x32-in, 12@28x28-out
-        << max_pool( 28, 28, 12, 2 ) // S2, 12@28x28-in, 12@14x14-out
-        << conv( 14, 14, 5, 12, 25 ) << relu() // C3, 12@14x14-in, 25@10x10-out
-        << max_pool( 10, 10, 25, 2 ) // S4, 25@10x10-in, 25@5x5-out
-        << fc( 625, 180 ) << relu() // F5, 625-in, 180-out
+    nn  << conv( 28+2*border_px, 28+2*border_px, 5, 1, 12 ) << relu() // C1, 1@28x28-in, 12@24x24-out
+        << max_pool( 24+2*border_px, 24+2*border_px, 12, 2 ) // S2, 12@24x24-in, 12@12x12-out
+        << conv( 12+border_px, 12+border_px, 5, 12, 25 ) << relu() // C3, 12@12x12-in, 25@8x8-out
+        << max_pool( 8+border_px, 8+border_px, 25, 2 ) // S4, 25@8x8-in, 25@4x4-out
+        << fc( 25*(8+border_px)*(8+border_px)/4, 180 ) << relu() // F5, 400-in, 180-out
         << dropout( 180, 0.5f )
         << fc( 180, 100 ) << relu() // F6, 180-in, 100-out
         << dropout( 100, 0.5f )
@@ -66,25 +66,32 @@ static void train_mnist(    const std::string &data_dir_path,
 
     construct_net( nn, backend_type );
 
-    std::cout << "load models..." << std::endl;
+    // for reproducibility
+    set_random_seed(7);
+
+    std::cout << "--> successfully loaded model" << std::endl;
 
     // load MNIST dataset
     std::vector<label_t> train_labels, test_labels;
     std::vector<vec_t> train_images, test_images;
 
-    parse_mnist_labels( data_dir_path + "/train-labels.idx1-ubyte", &train_labels);
-    parse_mnist_images( data_dir_path + "/train-images.idx3-ubyte", &train_images, -1.0, 1.0, 2, 2 );
-    parse_mnist_labels( data_dir_path + "/t10k-labels.idx1-ubyte", &test_labels);
-    parse_mnist_images( data_dir_path + "/t10k-images.idx3-ubyte", &test_images, -1.0, 1.0, 2, 2 );
+    parse_mnist_labels( data_dir_path + "/train-labels.idx1-ubyte", &train_labels );
+    parse_mnist_images( data_dir_path + "/train-images.idx3-ubyte", &train_images, -1.0, 1.0, border_px, border_px );
+    parse_mnist_labels( data_dir_path + "/t10k-labels.idx1-ubyte", &test_labels );
+    parse_mnist_images( data_dir_path + "/t10k-images.idx3-ubyte", &test_images, -1.0, 1.0, border_px, border_px );
 
-    std::cout << "start training" << std::endl;
+    // csv_parse_mnist( data_dir_path + "/train.csv", train_labels, train_images );
+    // csv_parse_mnist( data_dir_path + "/test.csv", test_labels, test_images );
+
+    std::cout << "--> training started" << std::endl;
 
     progress_display disp( train_images.size() );
     timer t;
 
-    // What is this for?
-    //optimizer.alpha *= std::min( tiny_dnn::float_t(4),
-    //    static_cast<tiny_dnn::float_t>( sqrt( n_minibatch ) * learning_rate ) );
+    // To avoid having to change the learning rate when the size of a mini-batch is changed,
+    // it is helpful to divide the total gradient computed on a mini-batch by the size of the mini-batch.
+    // This seems unimplemented in tiny-dnn, so we have to correct the learning rate depending on the batch size.
+    //optimizer.alpha /= 2;
 
     int epoch = 1;
 
@@ -93,8 +100,13 @@ static void train_mnist(    const std::string &data_dir_path,
         std::cout << "Epoch " << epoch << "/" << n_train_epochs << " finished. "
             << t.elapsed() << "s elapsed." << std::endl;
         ++epoch;
+
+        //tiny_dnn::result res = nn.test( train_images, train_labels );
+        //std::cout << "training score : " << res.num_success << "/" << res.num_total << std::endl;
+
         tiny_dnn::result res = nn.test( test_images, test_labels );
-        std::cout << res.num_success << "/" << res.num_total << std::endl;
+        auto score = static_cast<float_t>( 1000 * res.num_success / res.num_total ) / 10.f;
+        std::cout << "testing score : " << res.num_success << "/" << res.num_total << " - " << score << "%" << std::endl;
 
         disp.restart( train_images.size() );
         t.restart();
@@ -122,7 +134,7 @@ static void usage( const char *argv0 )
 int main( int argc, char **argv )
 {
     std::string data_path        = "";
-    int epochs                   = 30;
+    int epochs                   = 5;
     int minibatch_size           = 128;
     core::backend_t backend_type = core::default_engine();
 
