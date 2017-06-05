c6c5ad844c3d3f44436dbe031f8014bcafec0341
blackccpie <blackccpie@free.fr>
2017-06-05 18:39:27 +0200

    - default call install target for build scripts.

--- build_emcc.sh
+++ build_emcc.sh
@@ -4,4 +4,4 @@ mkdir -p build_emcc
 
 cd build_emcc
 cmake -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake -DUSE_EMSCRIPTEN=ON -DCMAKE_BUILD_TYPE=Release ..
-make -j3
+make -j3 install
--- build_gcc_avx2.sh
+++ build_gcc_avx2.sh
@@ -4,4 +4,4 @@ mkdir -p build_gcc
 
 cd build_gcc
 cmake -DUSE_AVX2=ON -DCMAKE_BUILD_TYPE=Release ..
-make -j3
+make -j3 install
