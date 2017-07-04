2e6c55ba58c86bf7bd118ec4876abe252f749777
blackccpie <blackccpie@free.fr>
2017-07-04 22:08:05 +0200

    - run script will default use python simple http server. Running with emrun implies more generation constraints.

--- apps/mnist_ocr/run.sh
+++ apps/mnist_ocr/run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-../../../emsdk-portable/emscripten/1.37.9/emrun --no_browser --port 8080 .
+#../../../emsdk-portable/emscripten/1.37.9/emrun --no_browser --port 8080 .
+python -c 'import SimpleHTTPServer; SimpleHTTPServer.test()'
